d8544b9e489275362e95591d088a2eb8f6880681
MANSOUR Malik <malik210999@gmail.com>
2025-04-12 16:32:45 +0200

    Image

--- app/components/QTRobot.tsx
+++ app/components/QTRobot.tsx
@@ -43,16 +43,17 @@ const QTRobot = ({ expression }: QTRobotProps) => {
   }, [expression]);
 
   return (
-    <div className="relative w-[300px] h-auto mb-8">
-      <img
-        src="https://docs.luxai.com/assets/images/qt_head-cd1f3ecbe79fb245a572462c68f363f1.png"
-        alt="QTrobot Container"
-        className="w-full h-auto"
-      />
+   <div className="relative w-[200px] h-auto mb-8">
+  <img
+    src="images/QT BoDy.png"
+    alt="QTrobot animation"
+    className="w-full h-auto"
+  />
+
             <img
         src={gifUrl}
         alt={`QT Robot Expression - ${expression}`}
-        className="absolute top-[81px] left-[43px] w-[210px] h-auto rounded-xl"
+        className="absolute top-[42px] left-[55px] w-[115px] h-auto rounded-xl"
       />
 
     </div>
